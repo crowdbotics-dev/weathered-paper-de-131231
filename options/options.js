e7299e1b376f3d5a70b66e69ce788c425b580ee8
Crowdbotics <engineering@crowdbotics.com>
2023-12-01 15:26:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-paper-de-131231",
+  url: "https://weathered-paper-de-131231.botics.co",
+  api: "https://weathered-paper-de-131231.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
